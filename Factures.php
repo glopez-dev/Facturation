0dec125465b2c1539a878b1738dae92add2be3b9
Gabriel <91618272+hlkgab@users.noreply.github.com>
2022-03-31 17:07:56 +0200

    Ajout de tout les fichiers de base du projet depuis le pc de Gabriel

---
+++ Factures.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Acceuil</title>
+
+    <meta charset="utf-8" />
+    <meta name="author" content="Gabriel Lopez" />
+    <meta lang="fr" />
+
+    <link href="css/base.css" rel="stylesheet" />
+    <link href="css/Facture.css" rel="stylesheet" />
+    <link href="#" rel="icon" />
+</head>
+
+<body>
+
+  <header>
+    <div class="logo">
+        <a href="index.php"><h1>M2L</h1></a>
+    </div>
+    <nav>
+        <ol>
+            <li><a href="index.php">Acceuil</a></li>  
+            <li><a href="Ligues.php">Ligues</a></li>
+            <li><a href="Prestations.php">Prestations</a></li>
+            <li class="current"><a href="Factures.php">Factures</a></li>
+        </ol>
+    </nav>
+  </header>
+
+  <section>
+      <div class="Table-Factures"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores corrupti soluta doloribus quae nesciunt hic. Reprehenderit quis accusamus consequuntur voluptates voluptatibus, neque tenetur magni dolorum eius temporibus ipsa suscipit nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugiat architecto rerum eaque minus! Voluptatem non rem animi ipsam nostrum, sequi, accusantium blanditiis dolorem vitae ipsum incidunt quae, doloremque eaque.</div>
+      <button type="button">Nouvelle Facture</button>
+  </section>
+
+</body>
+
+</html>
---
+++ Ligues.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ligues</title>
+
+    <meta charset="utf-8" />
+    <meta name="author" content="Gabriel Lopez" />
+    <meta lang="fr" />
+
+    <link href="css/base.css" rel="stylesheet" />
+    <link href="#" rel="icon" />
+    
+  </head>
+
+<body>
+
+  <header>
+    <div class="logo">
+        <a href="index.php"><h1>M2L</h1></a>
+    </div>
+    <nav>
+        <ol>
+            <li><a href="index.php">Acceuil</a></li>
+            <li class="current"><a href="Ligues.php">Ligues</a></li>
+            <li><a href="Prestations.php">Prestations</a></li>
+            <li><a href="Factures.php">Factures</a></li>
+        </ol>
+    </nav>
+  </header>
+
+  <section>
+  
+  </section>
+
+
+</body>
+
+</html>
+
